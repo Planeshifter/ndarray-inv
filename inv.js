c97b831e245e940f47546fc347b79c2e7c83f05d
Dirk Schumacher <mail@dirk-schumacher.net>
2017-11-13 20:06:29 +0100

    Use standard JS

--- inv.js
+++ inv.js
@@ -1,74 +1,74 @@
-var ndarray = require("ndarray");
-var zeros = require("ndarray-scratch").zeros;
+var ndarray = require('ndarray')
+var zeros = require('ndarray-scratch').zeros
 
-var inv = function inv(M){
+var inv = function inv (M) {
   // Gauss-Jordan elimination to invert 2d matrix
 
-  if(M.dimension !== 2){
-    throw new TypeError("Only matrices can be inverted, dimension has to be two.");
-  } else if (M.shape[0] !== M.shape[1]){
-    throw new TypeError("Matrix must be square");
+  if (M.dimension !== 2) {
+    throw new TypeError('Only matrices can be inverted, dimension has to be two.')
+  } else if (M.shape[0] !== M.shape[1]) {
+    throw new TypeError('Matrix must be square')
   }
 
-  var i = 0;
-  var ii = 0;
-  var j = 0;
-  var dim = M.shape[0];
-  var e = 0;
-  var t = 0;
-  var I = zeros([dim, dim]);
-  var C = ndarray(new Float64Array(dim*dim),[dim, dim]);
+  var i = 0
+  var ii = 0
+  var j = 0
+  var dim = M.shape[0]
+  var e = 0
+  var t = 0
+  var I = zeros([dim, dim])
+  var C = ndarray(new Float64Array(dim * dim), [dim, dim])
 
-  for(i=0; i<dim; i+=1){
-    for(j=0; j<dim; j+=1){
-      if(i === j){
-        I.set(i,j, 1);
+  for (i = 0; i < dim; i += 1) {
+    for (j = 0; j < dim; j += 1) {
+      if (i === j) {
+        I.set(i, j, 1)
       }
-      C.set(i,j, M.get(i,j) );
+      C.set(i, j, M.get(i, j))
     }
   }
 
-  for(i=0; i<dim; i+=1){
-    e = C.get(i,i);
+  for (i = 0; i < dim; i += 1) {
+    e = C.get(i, i)
 
-    if(e === 0){
-      for(ii=i+1; ii<dim; ii+=1){
-        if(C.get(ii, i) !== 0){
-          for(j=0; j<dim; j++){
-            e = C.get(i,j);
-            C.set(i, j, C.get(ii, j) );
-            C.set(ii, j, e);
-            e = I.get(i, j);
-            I.set(i, j, I.get(ii,j) );
-            I.set(ii, j, e);
+    if (e === 0) {
+      for (ii = i + 1; ii < dim; ii += 1) {
+        if (C.get(ii, i) !== 0) {
+          for (j = 0; j < dim; j++) {
+            e = C.get(i, j)
+            C.set(i, j, C.get(ii, j))
+            C.set(ii, j, e)
+            e = I.get(i, j)
+            I.set(i, j, I.get(ii, j))
+            I.set(ii, j, e)
           }
-          break;
+          break
         }
       }
-      e = C.get(i, i);
-      if(e === 0){
-        throw new Error("Matrix is not invertible");
+      e = C.get(i, i)
+      if (e === 0) {
+        throw new Error('Matrix is not invertible')
       }
     }
 
-    for(j=0; j<dim; j++){
-      C.set(i, j, C.get(i,j)/e);
-      I.set(i, j, I.get(i,j)/e);
+    for (j = 0; j < dim; j++) {
+      C.set(i, j, C.get(i, j) / e)
+      I.set(i, j, I.get(i, j) / e)
     }
 
-    for(ii=0; ii<dim; ii++){
-      if(ii==i){
-        continue;
+    for (ii = 0; ii < dim; ii++) {
+      if (ii == i) {
+        continue
       }
-      e = C.get(ii, i);
-      for(j=0; j<dim; j++){
-        C.set(ii, j, C.get(ii, j) - e*C.get(i, j) );
-        I.set(ii, j, I.get(ii, j) - e*I.get(i, j) );
+      e = C.get(ii, i)
+      for (j = 0; j < dim; j++) {
+        C.set(ii, j, C.get(ii, j) - e * C.get(i, j))
+        I.set(ii, j, I.get(ii, j) - e * I.get(i, j))
       }
     }
   }
   // I is now the inverse, return it
-  return I;
-};
+  return I
+}
 
-module.exports = inv;
+module.exports = inv
--- test/test.js
+++ test/test.js
@@ -1,43 +1,43 @@
-var chai = require('chai');
-var ndarray = require('ndarray');
-var inv = require('../inv.js');
-var expect = chai.expect;
+var chai = require('chai')
+var ndarray = require('ndarray')
+var inv = require('../inv.js')
+var expect = chai.expect
 
-describe("inv handles square matrices", function(){
-  it("returns identity for identity", function(){
-    var I = ndarray(new Float64Array([1,0,0,1]),[2,2]);
-    var I_inv = inv(I);
-    var I_arr = Array.prototype.slice.call(I);
-    var I_inv_arr = Array.prototype.slice.call(I_inv);
-    expect(I_inv_arr).to.eql(I_arr);
-  });
-  it("handles general 2x2 matrix", function(){
-    var I = ndarray(new Float64Array([2,1,3,4]),[2,2]);
-    var I_inv = inv(I);
-    expect(I_inv.get(0,0)).to.be.closeTo(0.8, 0.0001);
-    expect(I_inv.get(0,1)).to.be.closeTo(-0.2, 0.0001);
-    expect(I_inv.get(1,0)).to.be.closeTo(-0.6, 0.0001);
-    expect(I_inv.get(1,1)).to.be.closeTo(0.4, 0.0001);
-  });
-  it("throws error for singular matrices", function(){
-    var I = ndarray(new Float32Array([1,0,1,0]),[2,2]);
-    expect(function(){
-      inv(I);
-    }).to.throw(Error);
-  });
-});
+describe('inv handles square matrices', function () {
+  it('returns identity for identity', function () {
+    var I = ndarray(new Float64Array([1, 0, 0, 1]), [2, 2])
+    var I_inv = inv(I)
+    var I_arr = Array.prototype.slice.call(I)
+    var I_inv_arr = Array.prototype.slice.call(I_inv)
+    expect(I_inv_arr).to.eql(I_arr)
+  })
+  it('handles general 2x2 matrix', function () {
+    var I = ndarray(new Float64Array([2, 1, 3, 4]), [2, 2])
+    var I_inv = inv(I)
+    expect(I_inv.get(0, 0)).to.be.closeTo(0.8, 0.0001)
+    expect(I_inv.get(0, 1)).to.be.closeTo(-0.2, 0.0001)
+    expect(I_inv.get(1, 0)).to.be.closeTo(-0.6, 0.0001)
+    expect(I_inv.get(1, 1)).to.be.closeTo(0.4, 0.0001)
+  })
+  it('throws error for singular matrices', function () {
+    var I = ndarray(new Float32Array([1, 0, 1, 0]), [2, 2])
+    expect(function () {
+      inv(I)
+    }).to.throw(Error)
+  })
+})
 
-describe("other cases (non-square arrays, dim>2)", function(){
-  it("inv throws when dim>2", function(){
-    var I = ndarray(new Float64Array([1,2,3,4,5,6,7,8]),[2,2,2]);
-    expect(function(){
-      inv(I);
-    }).to.throw(TypeError);
-  });
-  it("inv throws when m != n", function(){
-    var I = ndarray(new Float64Array([1,2,3,4,5,6]),[2,3]);
-    expect(function(){
-      inv(I);
-    }).to.throw(TypeError);
-  });
-});
+describe('other cases (non-square arrays, dim>2)', function () {
+  it('inv throws when dim>2', function () {
+    var I = ndarray(new Float64Array([1, 2, 3, 4, 5, 6, 7, 8]), [2, 2, 2])
+    expect(function () {
+      inv(I)
+    }).to.throw(TypeError)
+  })
+  it('inv throws when m != n', function () {
+    var I = ndarray(new Float64Array([1, 2, 3, 4, 5, 6]), [2, 3])
+    expect(function () {
+      inv(I)
+    }).to.throw(TypeError)
+  })
+})
